11e5aca08fdff3535c9d418f30b9a0406d9741d0
Shubham Singh <shubhamsngh1295@gmail.com>
2021-07-17 17:33:00 +0530

    phase 2 : done with action creators

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+//action creators will return action as an object
+export const incNumber = () => {
+    return {type : "INCREMENT"};
+}
+
+export const decNumber = () => {
+    return {type : "DECREMENT"};
+}
