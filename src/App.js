29a4cc1c1e0349a351e36a64c37ebdc510eae551
Shubham Singh <shubhamsngh1295@gmail.com>
2021-07-17 17:57:38 +0530

    state being accessed using useSelector hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import  { useSelector } from 'react-redux';
 
 const App = () => {
+    let myState = useSelector((state) => { return state.changeTheNumber});
+    //CODE REVIEW : way of passing anonymous function in useSelector hook
     return(
         <>
         <div className="container_l_0">
             <div className="container_l_1">
                 <button> + </button>
-                <input type = "text"/>
+                <input type = "text" defaultValue = {myState}/>
                 <button> - </button>
             </div>
         </div>
