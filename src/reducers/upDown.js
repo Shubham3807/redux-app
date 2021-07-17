d80024765f25f1ee062cf16b6c0c09887f693ee8
Shubham Singh <shubhamsngh1295@gmail.com>
2021-07-17 18:31:04 +0530

    some minor changes--no impact on current fucntioning of app

--- src/reducers/upDown.js
+++ src/reducers/upDown.js
@@ -1,9 +1,9 @@
-let initialState = 0;
+const initialState = 0;
 
 const changeTheNumber = (state = initialState, action) => {
     switch(action.type){
-        case "INCREMENT" : return state+1;
-        case "DECREMENT" :return state-1;
+        case "INCREMENT" : return state + 1;
+        case "DECREMENT" :return state - 1;
         default : return state;
     }
 }
