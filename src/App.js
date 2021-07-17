d41e782d8604d11e534b72f11b5fd1d8736a5114
Shubham Singh <shubhamsngh1295@gmail.com>
2021-07-17 18:35:42 +0530

    actions dispatched successfully

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import  { useSelector } from 'react-redux';
+import  { useSelector, useDispatch } from 'react-redux';
+import {incNumber, decNumber} from './actions/index'
 
 const App = () => {
     let myState = useSelector((state) => { return state.changeTheNumber});
+    const dispatch = useDispatch();
     //CODE REVIEW : way of passing anonymous function in useSelector hook
     return(
         <>
         <div className="container_l_0">
             <div className="container_l_1">
-                <button> + </button>
-                <input type = "text" defaultValue = {myState}/>
-                <button> - </button>
+                <button onClick = {() => dispatch(incNumber())}> + </button>
+                <input type = "text" value = {myState}/>
+                <button onClick = {() => dispatch(decNumber())}> - </button>
             </div>
         </div>
         </>
